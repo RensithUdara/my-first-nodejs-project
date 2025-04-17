33288ef9058e700c0f54c375b745e5c2d7ab75a1
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-18 02:13:31 +0530

    Refactor events.js to improve script flow by adding console logs for better clarity

--- modules/events.js
+++ modules/events.js
@@ -2,10 +2,16 @@ const EventEmitter = require('node:events');
 
 const emitter = new EventEmitter();
 
+console.log('Start of script!');
+
 // Listen for the 'message' event
 emitter.on('message', (message) => {  // <-- Add the `message` parameter here
   console.log(`New message: ${message}`);
 });
 
+console.log('Pending script!');
+
 // Emit the 'message' event and pass the message as an argument
 emitter.emit('message', 'Hello, world!');
+
+console.log('End of script!');
