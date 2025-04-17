3fda8e32b9600552f983676c5134813913c9c273
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-17 18:45:58 +0530

    Comment out initial console logs and setTimeout implementation in timers.js

--- timers.js
+++ timers.js
@@ -1,10 +1,22 @@
-console.log('Hello World!');
+// console.log('Hello World!');
 
-const id = setTimeout(() => {
-    console.log('Hello after 2 seconds!');
+// const id = setTimeout(() => {
+//     console.log('Hello after 2 seconds!');
+// }
+//     , 2000);
+
+// clearTimeout(id);
+
+// console.log('End of script!');
+
+console.log('Start of script!');
+
+let count = 0;
+
+setInterval(() => {
+    console.log('Hello every 2 seconds! - ' + count);
+    count++;
 }
     , 2000);
-
-clearTimeout(id);
 
 console.log('End of script!');
