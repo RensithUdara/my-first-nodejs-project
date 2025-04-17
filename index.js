0c3a4ba5b925500a8e44f43847d615714c4be465
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-17 17:44:20 +0530

    Implement user input prompts using readline-sync and update package.json and package-lock.json

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const readline = require('readline');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-})
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// })
 
-rl.question('What is your name? ', (name) => {
-    console.log(`Hello ${name}!`);
-    rl.question('What is your age? ', (age) => {
-        console.log(`You are ${age} years old.`);
-        rl.close();
-    });
-});
+// rl.question('What is your name? ', (name) => {
+//     console.log(`Hello ${name}!`);
+//     rl.question('What is your age? ', (age) => {
+//         console.log(`You are ${age} years old.`);
+//         rl.close();
+//     });
+// });
+
+
+const readlinneSync = require('readline-sync');
+
+const name = readlinneSync.question('What is your name? ');
+console.log(`Hello ${name}!`);
+const age = readlinneSync.question('What is your age? ');
+console.log(`You are ${age} years old.`);
+const color = readlinneSync.question('What is your favorite color? ');
+console.log(`Your favorite color is ${color}.`); 
