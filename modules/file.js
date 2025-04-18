3f46cbe27f11ce9ddbc5df6767c413864c37f45f
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-19 00:57:06 +0530

    Refactor file.js to remove commented-out code and streamline file reading with promises

--- modules/file.js
+++ modules/file.js
@@ -1,16 +1,25 @@
-const fs = require('fs');
+//const fs = require('fs');
 
-console.log('Start of script!');
+// console.log('Start of script!');
 
-// Ensure the path to data.txt is correct
-fs.readFile('./modules/data.txt', 'utf-8' ,(err, data) => {
-    if (err) {
+// // Ensure the path to data.txt is correct
+// fs.readFile('./modules/data.txt', 'utf-8' ,(err, data) => {
+//     if (err) {
+//         console.error('Error reading file:', err);
+//         return;
+//     }
+//     console.log('File contents:', data.toString());
+// });
+
+// console.log('End of script!');
+
+const fs = require('fs').promises;
+
+
+fs.readFile('./modules/data.txt', 'utf-8')
+    .then((data) => {
+        console.log('File contents:', data);
+    })
+    .catch((err) => {
         console.error('Error reading file:', err);
-        return;
-    }
-    console.log('File contents:', data.toString());
 });
-
-console.log('End of script!');
-
-
