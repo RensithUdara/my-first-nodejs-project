8d8d1119aa63aa30cfcbb020bc0cd90ba2fccbdb
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-19 01:44:25 +0530

    update environment.js

--- modules/environment.js
+++ modules/environment.js
@@ -1,1 +1,4 @@
-console.log('Environment Variables:', process.env);
+const dotenv = require('dotenv');
+dotenv.config(); // Load environment variables from .env file
+
+console.log('Environment Variables: ', process.env); // Log all environment variables
