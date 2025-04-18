b9467c904f789bb120c7abae0273e09c566c6132
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-19 01:34:12 +0530

    update http.js

--- modules/http.js
+++ modules/http.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    fs.readFile('./modules/index.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Error reading file\n');
+            return;
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
 }).listen(8080, () => {
     console.log('Server running at http://localhost:8080/');
 });
