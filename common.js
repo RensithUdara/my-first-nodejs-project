3001199cb43eff6af5068ed6023d79c1a25555fe
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-19 23:56:50 +0530

    update common.js

---
+++ common.js
@@ -0,0 +1,6 @@
+import ("chalk").then((mod) => {
+console.log(mod.default.blue('Hello World!'));
+console.log(mod.default.red('Hello World!'));
+console.log(mod.default.green('Hello World!')); 
+
+})
