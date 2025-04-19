2dcf67ad2afae9e0ef0a6661519f5ab541803fcc
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-19 23:43:19 +0530

    update path.js

--- modules/path.js
+++ modules/path.js
@@ -8,4 +8,8 @@ const testPath = 'C:/Users/Rensith/OneDrive/Documents';
 
 // console.log(path.basename(testPath)); // Get the base name of the current file
 
-console.log(path.extname(__filename)); // Get the extension name of the current file
+// console.log(path.extname(__filename)); // Get the extension name of the current file
+
+// console.log(path.parse(__filename)); // Get the path object of the current file
+
+console.log(path.isAbsolute(testPath)); // Check if the path is absolute
