6c5d4953296df6c574696776fbb42cb84902545b
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-19 23:13:58 +0530

    update path.js

--- modules/path.js
+++ modules/path.js
@@ -1,5 +1,9 @@
 const path = require('path');
 
-console.log(__filename) ;
+const testPath = 'C:/Users/Rensith/OneDrive/Documents';
 
-console.log(__dirname) ;
+// console.log(__filename) ;
+
+// console.log(__dirname) ;
+
+console.log(path.basename(testPath)); // Get the base name of the current file
