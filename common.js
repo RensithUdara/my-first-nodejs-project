efd7703cb3fb67f848bafd86a32fb491c6f5bb6f
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-26 23:32:30 +0500

    build(deps): update common.js and package-lock.json

--- common.js
+++
@@ -1,8 +0,0 @@
-import ("chalk").then((mod) => {
-console.log(mod.default.blue('Hello World!'));
-console.log(mod.default.red('Hello World!'));
-console.log(mod.default.green('Hello World!')); 
-
-
-
-})
